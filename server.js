0b91f9814de895bf6f80a56ea08a3f4a6024d10c
Jason L <jkleitz27@gmail.com>
2022-06-24 16:49:34 -0400

    can query departments from db need to add other functions

--- server.js
+++ server.js
@@ -23,12 +23,12 @@ const promptUser = () => {
             type: 'list',
             name: 'choices', 
             message: 'What would you like to do?',
-            choices: ['View all departments', 
-                    'View all roles', 
-                    'View all employees', 
+            choices: ['View departments', 
+                    'View roles', 
+                    'View employees', 
                     'Add a department', 
                     'Add a role', 
-                    'Add an employee', 
+                    'Add a new employee', 
                     'Update an employee role',
                     'Update an employee manager',
                     "View employees by department",
@@ -36,8 +36,45 @@ const promptUser = () => {
                     'Delete a role',
                     'Delete an employee',
                     'View department budgets',
-                    'No Action']
+                    'Exit']
         }
     ])
+    .then(function(answer){
+      if(answer.choices === "View departments"){
+        viewDepartment();
+      }
+      else if(answer.choices === "View roles"){
+        viewRoles();
+      }
+      else if(answer.choices === "View employees"){
+        viewEmployees();
+      }
+      else if(answer.choices === "Add a department"){
+        addDepartment();
+      }
+      else if(answer.choices === "Add a role"){
+        addRole();
+      }
+      else if(answer.choices === "Add a new employee"){
+        addEmployee();
+      }
+      else if(answer.choices === "Update an employee role'"){
+        updateRole();
+      }
+      if(answer.choices === "Exit"){
+        connection.end();
+      }
+      
+      
+    })
+    
 }
 
+viewDepartment = ()=>{
+  console.log('Here are all of the departments')
+  connection.promise().query('SELECT department.id AS id, department.name AS department FROM department', (err,res) => {
+    if (err) throw err;
+    console.table(res);
+    promptUser();
+  })
+};
