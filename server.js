b0a9cb4bda80afbbc5219e9cde62320663858ffc
Jason L <jkleitz27@gmail.com>
2022-06-25 12:24:48 -0400

    can add department and roles now, need to add and update employees

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ const promptUser = () => {
         viewEmployees();
       }
       else if(answer.choices === "Add a department"){
-        addDepartment();
+        addDept();
       }
       else if(answer.choices === "Add a role"){
         addRole();
@@ -70,6 +70,7 @@ const promptUser = () => {
     
 }
 
+//view all departments
 viewDepartment = ()=>{
   console.log('Here are all of the departments')
   connection.promise().query('SELECT department.id AS id, department.name AS department FROM department', (err,res) => {
@@ -78,3 +79,124 @@ viewDepartment = ()=>{
     promptUser();
   })
 };
+
+//view all roles
+viewRoles = ()=>{
+  console.log('Here are all the roles')
+  connection.promise().query(`SELECT role.id, role.title, department.name AS department
+                                FROM role
+                                INNER JOIN department ON role.department_id = department.id`, (err,res) => {
+    if (err) throw err;
+    console.table(res);
+    promptUser();
+  })
+};
+
+//view all employess
+viewEmployees = ()=>{
+  console.log('Here are all the employess')
+  connection.promise().query(`SELECT employee.id, 
+                              employee.first_name, 
+                              employee.last_name, 
+                              role.title, 
+                              department.name AS department,
+                              role.salary, 
+                              CONCAT (manager.first_name, " ", manager.last_name) AS manager
+                            FROM employee
+                              LEFT JOIN role ON employee.role_id = role.id
+                              LEFT JOIN department ON role.department_id = department.id
+                              LEFT JOIN employee manager ON employee.manager_id = manager.id`, 
+                              (err,res) => {
+    if (err) throw err;
+    console.table(res);
+    promptUser();
+  })
+};
+
+//add department
+addDept = () => {
+  //Get departments from DB
+  connection.query('SELECT name FROM department', async (err,res) => {
+      //Prompt for what dept to add
+      if (err) throw err;
+      const data = await inquirer.prompt(
+          [  
+              {
+                  type: 'input',
+                  name: 'addDept',
+                  message: 'What department would you like too add?'
+              }
+          ]
+      );
+      //Add the new dept to the DB
+      connection.query('INSERT INTO department SET ?',
+          {
+              name: data.addDept
+          },
+          (err, res) => {
+              if (err) throw err;
+              //Success message
+              console.log(`\n${data.addDept} has been added to departments\n`);
+              promptUser();
+          }
+      );
+  });    
+}
+
+//add role
+addRole = () => {
+  //Get Departments from DB
+  connection.query('SELECT role.id, name, department_id, department.id FROM role RIGHT JOIN department ON role.department_id = department.id ORDER BY title ASC;', async (err, res) => {
+      if (err) throw err;
+      //Prompt for what role to add
+      const data = await inquirer.prompt(
+          [
+              {
+                  type: 'input',
+                  name: 'addRole',
+                  message: 'What role would you like too add?',
+              }, 
+              {
+                  type: 'input',
+                  name: 'addSalary',
+                  message: 'What is this roles salary?',
+                  validate: (value) => {
+                      const pass = value.match(/^[0-9]*$/);
+                      return pass ? true : 'Please enter a number'
+                  }
+              },
+              {
+                  type: 'list',
+                  name: 'addRoleDept',
+                  message: 'Which department is this for?',
+                  choices: () => {
+                      let roles = res.map(dept => dept.name);
+                      roles = [...new Set(roles)];
+                      return roles;
+                  }
+              }
+          ]
+      );
+      //Filter the dept name to get dept_id
+      let deptID;
+      res.filter(dept => {
+          if (dept.name === data.addRoleDept) {
+              deptID = dept.id;
+          }
+      });
+      //Add the role that the user has specified
+      connection.query('INSERT INTO role SET ?', 
+          {
+              title: data.addRole,
+              salary: data.addSalary,
+              department_id: deptID
+          },
+          (err, res) => {
+              if (err) throw err;
+              //Success message
+              console.log(`\n${data.addRole} has been added to roles\n`);
+              promptUser();
+          }
+      );
+  });
+}
