5de48c06a7e08c95e106f824d87e27f7f1e38270
Jason L <jkleitz27@gmail.com>
2022-06-25 18:10:58 -0400

    able to add employee but need to be able to update

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ addDept = () => {
               }
           ]
       );
-      //Add the new dept to the DB
+      //Add the new dept to the db
       connection.query('INSERT INTO department SET ?',
           {
               name: data.addDept
@@ -145,7 +145,7 @@ addDept = () => {
 
 //add role
 addRole = () => {
-  //Get Departments from DB
+  //Get Departments from db
   connection.query('SELECT role.id, name, department_id, department.id FROM role RIGHT JOIN department ON role.department_id = department.id ORDER BY title ASC;', async (err, res) => {
       if (err) throw err;
       //Prompt for what role to add
@@ -195,6 +195,90 @@ addRole = () => {
               if (err) throw err;
               //Success message
               console.log(`\n${data.addRole} has been added to roles\n`);
+              promptUser();
+          }
+      );
+  });
+};
+
+//add employee
+addEmployee = () => {
+  connection.query('SELECT role.id, role.title, CONCAT(employee2.first_name," ", employee2.last_name) AS manager, employee.manager_id FROM role LEFT JOIN employee on employee.role_id = role.id LEFT JOIN employee AS employee2 ON employee.manager_id = employee2.id;', async (err, res) => {
+      if (err) throw err;
+      //Prompt for employee data to add
+      const data = await inquirer.prompt(
+          [
+              {
+                  type: 'input',
+                  name: 'addFirst',
+                  message: 'What is their first name?',
+              },
+              {
+                  type: 'input',
+                  name: 'addLast',
+                  message: 'What is their last name?',
+              },
+              {
+                  type: 'list',
+                  name: 'addRole',
+                  message: 'What is their role?',
+                  choices: () => {
+                      let roles = res.map(role => role.title);
+                      roles = [...new Set(roles)];
+                      return roles;
+                  }
+                  
+              },
+              {
+                  type: 'confirm',
+                  name: 'hasMGR',
+                  message: 'Will this employee be managed?'
+              },
+              {
+                  type: 'list',
+                  name: 'manager',
+                  message: 'Please choose their manager',
+                  when: answers => answers.hasMGR,
+                  choices: () => {
+                      let managers = [];
+                      res.filter(role => {
+                          if (typeof role.manager === 'string') {
+                              managers.push(role.manager)
+                          }
+                      });
+                      managers = [...new Set(managers)]
+                      return managers;
+                  }
+              }
+          ]
+      );
+      //Filter the chosen role to get its role_id
+      let roleID;
+      res.filter(role => {
+          if(role.title === data.addRole){
+              roleID = role.id;
+          }
+      });
+      //Filter the chosen manger and assign manager_id
+      let managerID;
+      res.filter(role => {
+          if (role.manager === data.manager){
+              managerID = role.manager_id;
+          }
+      });
+      //Add the employee as specified 
+      connection.query('INSERT INTO employee SET ?',
+          {
+              first_name: data.addFirst,
+              last_name: data.addLast,
+              role_id: roleID,
+              manager_id: managerID
+
+          },
+          (err, res) => {
+              if (err) throw err;
+              //Success message
+              console.log(`\n${data.addFirst} ${data.addLast} has been added as an employee\n`);
               promptUser();
           }
       );
